699d96f9a57b508b07dea7bf2da2fa84021a6e88
leehyoenjong <99190990+leehyoenjong@users.noreply.github.com>
2024-06-07 19:46:04 +0900

    수정

--- Assets/scripts/Monster/LongNote.cs
+++ Assets/scripts/Monster/LongNote.cs
@@ -2,12 +2,60 @@ using UnityEngine;
 
 public class LongNote : MonoBehaviour
 {
-    private void Update()
-    {   
-        
+    [SerializeField] Transform Tr;
+    [SerializeField] GameObject G_Effect;
+    [SerializeField] GameObject G_End;
+
+    GameObject Effect;
+
+    float Dealy = 0f;
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag != "Player")
+        {
+            return;
+        }
+
         if (PlayerController.CheckHold)
         {
+            Dealy -= Time.deltaTime;
+            if (Dealy <= 0)
+            {
+                AudioManager.instance.PlaySound();
+                Dealy = 0.1f;
+            }
 
+            if (Effect == null)
+            {
+                var createpos = GameManager.instance.skeleton.transform.position;
+                createpos.x += 1;
+                createpos.y = 0;
+                Effect = Instantiate(G_Effect, createpos, default, null);
+            }
+
+            var scale = Tr.localScale;
+            scale.x -= 0.14f;
+
+            Tr.localScale = scale;
+
+            if (Tr.localScale.x <= 0)
+            {
+                Destroy(this.gameObject);
+                Destroy(Effect);
+                var createpos = GameManager.instance.skeleton.transform.position;
+                createpos.x += 1;
+                createpos.y = 0;
+                var end = Instantiate(G_End, createpos, default, null);
+                Destroy(end, 1f);
+            }
+            return;
         }
+        if (Effect)
+        {
+            Destroy(Effect);
+        }
+
+        var box = Tr.GetComponent<BoxCollider2D>();
+        box.enabled = false;
     }
 }
