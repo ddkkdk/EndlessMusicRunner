f13f6b9398b3d31a23f5a571aa10390d4219c2b4
이승우 <chrisnarulee123@gamil.com>
2024-06-07 09:50:21 +0900

    작업 1. 화면 설정 / 2. 조작 /3. 몬스터 사망 판정 작업진행중(1. Screen setting / 2. Manipulation / 3. Monster death determination in progress) 1번 작업 완료 (1 is complete) 2 번 3번 작업 진행해야함

---
+++ Assets/@Scripts/Data/Data.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameData
+{
+  
+    public class Data : MonoBehaviour
+    {
+        #region 레벨디자인 테이블 (LevelDesigin Table)
+        static Dictionary<int, LevelDesign> _LevelDesign;
+        public static Dictionary<int, LevelDesign> LevelDesigin
+        {
+            get 
+            { 
+                return _LevelDesign; 
+            }
+        }
+        #endregion
+    }
+
+}
