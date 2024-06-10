b48c39212dd7d95810acc93b133593c3d7ce0551
이승우 <chrisnarulee123@gamil.com>
2024-06-10 17:14:31 +0900

    메인게임씬 롱노트 없는 상태

--- Assets/@Scripts/Data/DataTable_Class/LevelDesign.cs
+++ Assets/@Scripts/Data/DataTable_Class/LevelDesign.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class LevelDesign
 {
-    
+    public int[] M_ID;
+    public int[] Stage_Info;
+    public int[] MonsterInfo; // -> MonsterTable 코드로 변경해야함.
 }
 public class C_LevelDesign
 {
---
+++ Assets/@Scripts/LSW TestSCripts/MySpwanManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MySpwanManager : MonoBehaviour
+{
+
+    #region Change Addressables Code
+    public List<Monster> monsterList = new List<Monster>(); 
+    public List<Boss> bossList =new List<Boss>();
+    #endregion
+
+
+}
