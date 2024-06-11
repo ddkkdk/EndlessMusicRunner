3d5c3944d640809141149f9d57ca143b0dc8da57
이승우 <chrisnarulee123@gamil.com>
2024-06-11 10:10:36 +0900

    PlayerSkin변경이되게 만듬

---
+++ Assets/scripts/Player/ChangePlayerSkin.cs
@@ -0,0 +1,23 @@
+using Spine.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePlayerSkin : MonoBehaviour
+{
+   public List<string> skin_Names = new List<string>();
+    SkeletonAnimation skeletonAnimation;
+    public void Awake()
+    {
+        skeletonAnimation = GetComponent<SkeletonAnimation>();
+        SetPlayerSkin();
+    }
+
+
+    public void SetPlayerSkin()
+    {
+        skeletonAnimation.Skeleton.SetSkin(skin_Names[(int)UI_Lobby.playerSkinType]);
+        skeletonAnimation.Skeleton.SetSlotsToSetupPose();
+        skeletonAnimation.AnimationState.Apply(skeletonAnimation.Skeleton);
+    }
+}
