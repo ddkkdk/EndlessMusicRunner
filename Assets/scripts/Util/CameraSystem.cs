4935415b5845b60cbddfd0a68f9880c66156a22d
leehyoenjong <99190990+leehyoenjong@users.noreply.github.com>
2024-06-12 21:06:34 +0900

    카메라 수정

--- Assets/scripts/Util/CameraSystem.cs
+++ Assets/scripts/Util/CameraSystem.cs
@@ -6,24 +6,29 @@ using UnityEngine;
 public class CameraSystem : MonoBehaviour
 {
     public static CameraSystem cameraSystem;
-    [SerializeField] Camera camera_main;
 
-    [SerializeField] Vector3 orginPos;
-    [SerializeField] float orginSize;
+    //기존 위치
+    Vector3 orginPos = new Vector3(-44.5f, 3.05f, -26.1f);
+    float orginSize = 9;
 
-    [SerializeField] Vector3 ZoomInPos;
-    [SerializeField] float ZoomInSize;
+    //줌인 사이즈 및 위치
+    Vector3 ZoomInPos = new Vector3(-48, 3.05f, -26.1f);
+    float ZoomInSize = 7;
+    float Up_Down_Szie = 7;
 
-    [SerializeField] float Up_Down_Szie;
-
+    //타겟
     float targetSize;
     Vector3 target;
+
+    //카메라
+    Camera camera_main;
 
     private void Awake()
     {
         cameraSystem = this;
         targetSize = orginSize;
         target = this.transform.position;
+        camera_main = Camera.main;
     }
 
     private void Update()
